fefbdc239d8003a9cbfd8594c242b6a6193b84f5
Diego Tolotto <diego.tolotto8@gmail.com>
2022-06-20 06:50:08 -0300

    Finalizando os detalhes no README e retirando o arquivo database

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -8,9 +8,9 @@ class Controller {
         app.get('/users', async (req, res) => {
             try {
                 const resposta = await Metodos.listaClientes();
-                res.status(200).json({mensagem: resposta});
+                res.status(200).json({ mensagem: resposta });
             } catch (error) {
-                res.status(400).json({erro: error.message});
+                res.status(400).json({ erro: error.message });
             }
         });
 
@@ -18,27 +18,28 @@ class Controller {
             try {
                 const id = req.params.id;
                 const resposta = await Metodos.listaClientePorId(id);
-                res.status(200).json({mensage: resposta})
+                res.status(200).json({ mensage: resposta })
             } catch (error) {
-                res.status(400).json({erro: error.message});
-            }   
+                res.status(400).json({ erro: error.message });
+            }
         });
+
 
         app.post('/users', async (req, res) => {
             const valido = Validacoes.validaNome(req.body.nome) &&
-                           Validacoes.validaEmail(req.body.email) &&
-                           Validacoes.validaTelefone(req.body.telefone);
+                Validacoes.validaEmail(req.body.email) &&
+                Validacoes.validaTelefone(req.body.telefone);
             try {
-                if(valido) {
+                if (valido) {
                     const cliente = new ClienteModel(...Object.values(req.body));
                     const resposta = await Metodos.insereCliente(cliente);
-                    res.status(201).json({mensagem: resposta});
+                    res.status(201).json({ mensagem: resposta });
                 } else {
                     throw new Error("Pedido invalido.");
                 }
-                    
+
             } catch (error) {
-                res.status(400).json({error: error.message})             
+                res.status(400).json({ error: error.message })
             }
         })
 
@@ -47,20 +48,20 @@ class Controller {
                 const id = req.params.id;
                 const clienteAtualizado = new ClienteModel(...Object.values(req.body));
                 const resposta = await Metodos.atualizaPorId(id, clienteAtualizado);
-                res.status(200).json({mensagem: resposta});
+                res.status(200).json({ mensagem: resposta });
             } catch (error) {
-                res.status(400).json({erro: error.message});
+                res.status(400).json({ erro: error.message });
             }
         });
 
         app.delete('/users/:id', async (req, res) => {
             try {
                 const id = req.params.id;
-                const resposta = await Metodos.deletaPorId(id); 
-                res.status(200).json({mensagem: resposta});
+                const resposta = await Metodos.deletaPorId(id);
+                res.status(200).json({ mensagem: resposta });
             } catch (error) {
-                res.status(400).json({erro: error.message});
-            }            
+                res.status(400).json({ erro: error.message });
+            }
         });
     }
 };
